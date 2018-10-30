5ec2fd5e922fa3612d7c4498975ff3a82b20c1a1
Administrator <Administrator@PC-20180411FDOU>
2018-10-30 08:44:54 +0800

    add util package classes

---
+++ src/test/java/PropsTest.java
@@ -0,0 +1,12 @@
+import com.hncy58.util.PropsUtil;
+
+public class PropsTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println(PropsUtil.get("kafka-to-hdfs.agentSvrName"));
+		System.out.println(PropsUtil.get("kafka-to-hdfs", "agentSvrName"));
+		System.out.println(PropsUtil.getWithDefault("kafka-to-hdfs.agentSvrName", "abc"));
+		System.out.println(PropsUtil.getWithDefault("kafka-to-hdfs", "agentSvrName", "def"));
+	}
+}
